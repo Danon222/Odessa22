d6c5503494df4bd5bf5677d52cee6ff99180bc4a
sergeykudlay <kudlaysv88@gmail.com>
2022-06-07 13:00:28 +0300

    add file

---
+++ main.py
@@ -0,0 +1,15 @@
+import this
+import antigravity
+import __hello__
+
+from art import *
+tprint("Sergiy")
+
+x=13
+y=5
+print(x,y)
+print(x,"+",y,"=",x+y)
+print(x,"//",y,"=",x//y)
+print(x,"%",y,"=",x%y)
+print(x,"**",y,"=",x**y)
+
