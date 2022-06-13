aa5b4a9c2339c24ddaa70ca2a0eaae453190de9a
sergeykudlay <kudlaysv88@gmail.com>
2022-06-13 12:53:22 +0300

    update

---
+++ Lesson2/Lesson 2 Задание 6.py
@@ -0,0 +1,4 @@
+some_range = list(range(10, 101, 10))
+print(some_range)
+X = float(input("Ввести число:"))
+print(abs(X) in some_range)
---
+++ Lesson2/Lesson 2 Задание 7.py
@@ -0,0 +1,3 @@
+a = int(input('Input number of stars:'))
+for i in range(1):
+    print(a * '*')
