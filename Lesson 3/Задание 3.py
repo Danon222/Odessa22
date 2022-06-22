82fb149129349c993c6e2770c6efce045295fd7e
sergeykudlay <kudlaysv88@gmail.com>
2022-06-22 12:06:33 +0300

    update

--- Lesson 3/Задание 3.py
+++ Lesson 3/Задание 3.py
@@ -1,3 +1,5 @@
+"""Друкувати список list_ten = [10, 20, 30, 40, 50] реверсивно - тобто. програма має повернути [50, 40, 30, 20, 10]."""
+
 some_range = list(range(10, 51, 10))
 some_range2 = list(reversed(some_range))
 print(some_range)
